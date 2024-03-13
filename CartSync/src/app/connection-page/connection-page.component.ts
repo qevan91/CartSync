e553590fbd75f47e033944a7ba03a8aa307bdbb5
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-03-13 11:58:17 +0100

    'ng g component connection-page' and 'ng g component main-page'

---
+++ CartSync/src/app/connection-page/connection-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './connection-page.component.html',
+  styleUrl: './connection-page.component.css'
+})
+export class ConnectionPageComponent {
+
+}
